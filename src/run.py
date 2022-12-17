20d948f5ed7ef097995ec85f7bf276aba3f3e172
Dennis Willie <denniswillie2000@gmail.com>
2022-12-17 12:41:07 +0000

    Train classifier model 2: 23% acc on val data

--- src/run.py
+++ src/run.py
@@ -6,14 +6,25 @@ from ml_module.models.classifier_model_2 import Classifier_Model_2
 
 (x_train, y_train, train_crop), (x_val, y_val, val_crop) = extract_data()
 
-x_train, y_train, x_val, y_val = preprocess(x_train, x_val, train_crop, val_crop, y_train, y_val)
-
+x_train, y_train, x_val, y_val = preprocess(
+    x_train, 
+    x_val, 
+    train_crop, 
+    val_crop, 
+    y_train, 
+    y_val,
+    box_apply_type="None",
+    color_channel="RGB",
+    blur=False,
+    hist_equalization=False,
+    resize_to=(64, 64)
+)
 
 # model1 = Classifier_Model_1()
 # model1.train(x_train, y_train, x_val, y_val)
 
-x_train = x_train.reshape(len(x_train), 64, 64, 1)
-x_val = x_val.reshape(len(x_val), 64, 64, 1)
-
 model2 = Classifier_Model_2()
 model2.train(x_train, y_train, x_val, y_val)
+
+print(model2.summary())
+model2.save('./')
