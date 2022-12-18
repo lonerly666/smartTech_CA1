ee3a7dba5d9e0c037c5e6088a0ca379390956331
Dennis Willie <denniswillie2000@gmail.com>
2022-12-18 22:06:45 +0000

    hotfix: predictions.txt now have the file names

--- src/run.py
+++ src/run.py
@@ -8,71 +8,71 @@ from matplotlib import pyplot as plt
 from utils import translate_predictions
 import random
 
-(x_train, y_train, train_crop), (x_val, y_val, val_crop), x_test, labels_indexes, labels_english = extract_data()
+(x_train, y_train, train_crop), (x_val, y_val, val_crop), x_test, labels_indexes, labels_english, test_names_indexes = extract_data()
 
 explore_data(x_train, y_train, x_val, y_val, x_test, train_crop, val_crop, labels_indexes)
 
-#LeNet model
-m1_x_train, m1_y_train, m1_x_val, m1_y_val, _ = preprocess(
-    x_train,
-    x_val,
-    x_test,
-    train_crop,
-    val_crop,
-    y_train,
-    y_val,
-    labels_indexes,
-    box_apply_type="None",
-    color_channel="GRAY",
-    blur=True,
-    hist_equalization=True,
-    resize_to=(64, 64))
-lenet_model = Classifier_Model_1()
-lenet_model.train(m1_x_train, m1_y_train, m1_x_val, m1_y_val)
-print(lenet_model.summary())
-lenet_model.save("./src/ml_module/saved/lenet.h5")
+# #LeNet model
+# m1_x_train, m1_y_train, m1_x_val, m1_y_val, _ = preprocess(
+#     x_train,
+#     x_val,
+#     x_test,
+#     train_crop,
+#     val_crop,
+#     y_train,
+#     y_val,
+#     labels_indexes,
+#     box_apply_type="None",
+#     color_channel="GRAY",
+#     blur=True,
+#     hist_equalization=True,
+#     resize_to=(64, 64))
+# lenet_model = Classifier_Model_1()
+# lenet_model.train(m1_x_train, m1_y_train, m1_x_val, m1_y_val)
+# print(lenet_model.summary())
+# lenet_model.save("./src/ml_module/saved/lenet.h5")
 
 
-# Modified LeNet model
-m2_x_train, m2_y_train, m2_x_val, m2_y_val, _ = preprocess(
-    x_train,
-    x_val,
-    x_test,
-    train_crop,
-    val_crop,
-    y_train,
-    y_val,
-    labels_indexes,
-    box_apply_type="DiscardZeroArea",
-    color_channel="GRAY",
-    blur=True,
-    hist_equalization=True,
-    resize_to=(32, 32))
-modified_lenet_model = Classifier_Model_3()
-modified_lenet_model.train(m2_x_train, m2_y_train, m2_x_val, m2_y_val)
-print(modified_lenet_model.summary())
-modified_lenet_model.save("./src/ml_module/saved/modified_lenet.h5")
+# # Modified LeNet model
+# m2_x_train, m2_y_train, m2_x_val, m2_y_val, _ = preprocess(
+#     x_train,
+#     x_val,
+#     x_test,
+#     train_crop,
+#     val_crop,
+#     y_train,
+#     y_val,
+#     labels_indexes,
+#     box_apply_type="DiscardZeroArea",
+#     color_channel="GRAY",
+#     blur=True,
+#     hist_equalization=True,
+#     resize_to=(32, 32))
+# modified_lenet_model = Classifier_Model_3()
+# modified_lenet_model.train(m2_x_train, m2_y_train, m2_x_val, m2_y_val)
+# print(modified_lenet_model.summary())
+# modified_lenet_model.save("./src/ml_module/saved/modified_lenet.h5")
 
-# Modified AlexNet model
-m3_x_train, m3_y_train, m3_x_val, m3_y_val, _ = preprocess(
-    x_train,
-    x_val,
-    x_test,
-    train_crop,
-    val_crop,
-    y_train,
-    y_val,
-    labels_indexes,
-    box_apply_type="None",
-    color_channel="RGB",
-    blur=False,
-    hist_equalization=True,
-    resize_to=(64, 64))
-alexnet_model = Classifier_Model_2()
-alexnet_model.build()
-alexnet_model.train(m3_x_train, m3_y_train, m3_x_val, m3_y_val)
-print(alexnet_model.summary())
-alexnet_model.save("./src/ml_module/saved/modified_alexnet.h5")
+# # Modified AlexNet model
+# m3_x_train, m3_y_train, m3_x_val, m3_y_val, _ = preprocess(
+#     x_train,
+#     x_val,
+#     x_test,
+#     train_crop,
+#     val_crop,
+#     y_train,
+#     y_val,
+#     labels_indexes,
+#     box_apply_type="None",
+#     color_channel="RGB",
+#     blur=False,
+#     hist_equalization=True,
+#     resize_to=(64, 64))
+# alexnet_model = Classifier_Model_2()
+# alexnet_model.build()
+# alexnet_model.train(m3_x_train, m3_y_train, m3_x_val, m3_y_val)
+# print(alexnet_model.summary())
+# alexnet_model.save("./src/ml_module/saved/modified_alexnet.h5")
 
 # Chosen AlexNet model.
 _, _, _, _, preprocessed_x_test = preprocess(
@@ -96,6 +96,6 @@ chosen_alexnet_model.load("./src/ml_module/saved/modified_alexnet.h5")
 # Predict the test dataset using the chosen alexnet model.
 with open('./predictions.txt', 'w') as f:
     predictions = chosen_alexnet_model.predict(preprocessed_x_test)
-    translated_predictions = translate_predictions(predictions, labels_indexes, labels_english)
+    translated_predictions = translate_predictions(predictions, labels_indexes, labels_english, test_names_indexes)
     for prediction in translated_predictions:
-        f.write("{} {}\n".format(prediction[0], prediction[1]))
+        f.write("{} {} {}\n".format(prediction[0], prediction[1], prediction[2]))
--- src/utils.py
+++ src/utils.py
@@ -2,16 +2,16 @@ import numpy as np
 import copy
 
 # Given a list of predictions in the form of softmax probabilities, generate a list 
-# of lists in the form of [n*** label string, english words label]
-def translate_predictions(predictions, labels_indexes, labels_english):
+# of lists in the form of [image_name, n*** label string, english words label]
+def translate_predictions(predictions, labels_indexes, labels_english, test_names_indexes):
 	# swap labels_indexes (key becomes value, value becomes key)
 	swap_labels_indexes = copy.deepcopy(labels_indexes)
 	swap_labels_indexes = {v: k for k, v in labels_indexes.items()}
 
 	result = []
-	for prediction in predictions:
+	for i, prediction in enumerate(predictions):
 		index = np.argmax(prediction, axis=0)
 		n_prefix = swap_labels_indexes[index]
-		result.append((n_prefix, labels_english[n_prefix]))
+		result.append((test_names_indexes[i], n_prefix, labels_english[n_prefix]))
 
 	return result
